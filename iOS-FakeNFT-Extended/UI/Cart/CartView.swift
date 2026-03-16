f5ead8226063f404258582aa99bc95d7126280f4
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-16 19:43:46 +0300

    [Add] CartView header layout implementation

--- iOS-FakeNFT-Extended/UI/Cart/CartView.swift
+++ iOS-FakeNFT-Extended/UI/Cart/CartView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct CartView: View {
+    @State private var showSortOptions = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    showSortOptions = true
+                } label: {
+                    Image(.sort)
+                }
+                .padding(.trailing, 10)
+                .frame(width: 42, height: 42)
+                .buttonStyle(.plain)
+                .confirmationDialog("ActionSheet.sorting.title", isPresented: $showSortOptions, titleVisibility: .visible) {
+                    Button("ActionSheet.sorting.price") {
+                        
+                    }
+                    Button("ActionSheet.sorting.rating") {
+                        
+                    }
+
+                    Button("ActionSheet.sorting.name") {
+                        
+                    }
+                    
+                    Button("ActionSheet.sorting.close", role: .cancel) {
+                                    
+                    }
+                }
+            }
+            
+            Spacer()
+        }
     }
 }
 
