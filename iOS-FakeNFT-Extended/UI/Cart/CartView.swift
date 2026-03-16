f22799978229fa1aee4a16fabad2f7465950cebc
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-16 11:03:03 +0300

    [Add] colors assets/CartView definition

---
+++ iOS-FakeNFT-Extended/UI/Cart/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 13.03.2026.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
