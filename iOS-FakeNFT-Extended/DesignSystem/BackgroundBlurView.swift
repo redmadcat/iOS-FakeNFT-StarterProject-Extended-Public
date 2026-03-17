0c26b1f11db9b33f4038fc83556e2c4fbd8f40c8
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-17 16:07:18 +0300

    [Fix] project group structure

---
+++ iOS-FakeNFT-Extended/DesignSystem/BackgroundBlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundBlurView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 17.03.2026.
+//
+
+import SwiftUI
+
+struct BackgroundBlurView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
--- iOS-FakeNFT-Extended/UI/NftCard/NftCardDeleteCover.swift
+++ iOS-FakeNFT-Extended/UI/NftCard/NftCardDeleteCover.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct NftCardDeleteCover: View {
+    @Binding var showDeleteCover: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(.deleteStub)
+
+            Text("NftCardItemCell.delete.title")
+                .multilineTextAlignment(.center)
+                .font(.bodyRegular13)
+                .frame(width: 180)
+                .padding(.top, 12)
+            HStack {
+                Button {
+                } label: {
+                    Text("NftCardItemCell.delete.accept")
+                        .font(.bodyRegular17)
+                        .foregroundStyle(.ypRed)
+                }
+                .frame(width: 127, height: 44)
+                .background(.ypBlackAD)
+                .buttonStyle(.plain)
+                .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+                
+                Button {
+                    showDeleteCover.toggle()
+                } label: {
+                    Text("NftCardItemCell.delete.cancel")
+                        .font(.bodyRegular17)
+                        .foregroundStyle(.ypWhiteAD)
+                }
+                .frame(width: 127, height: 44)
+                .background(.ypBlackAD)
+                .buttonStyle(.plain)
+                .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+            }
+            .padding(.top, 20)
+        }
+        .frame(
+              minWidth: 0,
+              maxWidth: .infinity,
+              minHeight: 0,
+              maxHeight: .infinity,
+              alignment: .center
+            )
+        .background(BackgroundBlurView())
     }
 }
 
 #Preview {
-    NftCardDeleteCover()
+    NftCardDeleteCover(showDeleteCover: .constant(false))
 }
