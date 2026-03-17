8ba6613de89f790aadac0a6a1d5dd180e27eceb2
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-17 22:33:59 +0300

    [Add] CartEmptyView layout implementation

---
+++ iOS-FakeNFT-Extended/UI/Cart/CartEmptyView.swift
@@ -0,0 +1,30 @@
+//
+//  CartEmptyView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 17.03.2026.
+//
+
+import SwiftUI
+
+struct CartEmptyView: View {
+    var body: some View {
+        VStack {
+            Text("CartEmptyView.title")
+                .font(.bodyBold17)
+                .foregroundStyle(.ypBlackAD)
+        }
+        .frame(
+              minWidth: 0,
+              maxWidth: .infinity,
+              minHeight: 0,
+              maxHeight: .infinity,
+              alignment: .center
+            )
+        .background(.ypWhiteAD)
+    }
+}
+
+#Preview {
+    CartEmptyView()
+}
