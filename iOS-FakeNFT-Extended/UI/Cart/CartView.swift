8056575d4bb4aedc5559ff6e7f3c50619f5e6acb
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-17 13:24:51 +0300

    [Add] CartView NFT collection implementation

--- iOS-FakeNFT-Extended/UI/Cart/CartView.swift
+++ iOS-FakeNFT-Extended/UI/Cart/CartView.swift
@@ -14,35 +14,58 @@ struct CartView: View {
         VStack {
             HStack {
                 Spacer()
-                
-                Button {
-                    showSortOptions = true
-                } label: {
-                    Image(.sort)
-                }
-                .padding(.trailing, 10)
-                .frame(width: 42, height: 42)
-                .buttonStyle(.plain)
-                .confirmationDialog("ActionSheet.sorting.title", isPresented: $showSortOptions, titleVisibility: .visible) {
-                    Button("ActionSheet.sorting.price") {
-                        
-                    }
-                    Button("ActionSheet.sorting.rating") {
-                        
-                    }
-
-                    Button("ActionSheet.sorting.name") {
-                        
-                    }
-                    
-                    Button("ActionSheet.sorting.close", role: .cancel) {
-                                    
-                    }
-                }
+                sortButton
             }
             
+            nftCardList
             Spacer()
         }
+        .background(.ypWhiteAD)
+    }
+    
+    private var sortButton: some View {
+        Button {
+            showSortOptions = true
+        } label: {
+            Image(.sort).renderingMode(.template)
+        }
+        .padding(.trailing, 10)
+        .frame(width: 42, height: 42)
+        .buttonStyle(.plain)
+        .confirmationDialog("ActionSheet.sorting.title", isPresented: $showSortOptions, titleVisibility: .visible) {
+            Button("ActionSheet.sorting.price") {
+                
+            }
+            Button("ActionSheet.sorting.rating") {
+                
+            }
+
+            Button("ActionSheet.sorting.name") {
+                
+            }
+            
+            Button("ActionSheet.sorting.close", role: .cancel) {
+                            
+            }
+        }
+    }
+    
+    private var nftCardList: some View {
+        List(NftCardItem.mockItems) { item in
+            NftCardItemCell(item: item)
+                .listRowBackground(Color.ypWhiteAD)
+                .listRowSeparator(.hidden)
+                .listRowInsets(
+                    EdgeInsets(
+                        top: 16,
+                        leading: 0,
+                        bottom: 16,
+                        trailing: 0
+                    )
+                )
+        }
+        .scrollContentBackground(.hidden)
+        .listRowSpacing(0)
     }
 }
 
