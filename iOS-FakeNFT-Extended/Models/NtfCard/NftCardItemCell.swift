b4ba295349ef1673c72af95b7d27c50ff68f3a27
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-17 12:21:55 +0300

    [Add] Rating0 image asset

---
+++ iOS-FakeNFT-Extended/Models/NtfCard/NftCardItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  NftCardItemCell.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 17.03.2026.
+//
+
+import SwiftUI
+
+struct NftCardItemCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NftCardItemCell()
+}
