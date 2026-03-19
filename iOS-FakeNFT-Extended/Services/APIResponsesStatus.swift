e84af9bf505d4c2f4765fa1bca1af94657f5656c
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-19 21:33:02 +0300

    [Add] APIResponsesStatus implementation

---
+++ iOS-FakeNFT-Extended/Services/APIResponsesStatus.swift
@@ -0,0 +1,13 @@
+//
+//  APIResponsesStatus.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 19.03.2026.
+//
+
+enum APIResponseStatus {
+    case `default`
+    case loading
+    case success
+    case failure
+}
