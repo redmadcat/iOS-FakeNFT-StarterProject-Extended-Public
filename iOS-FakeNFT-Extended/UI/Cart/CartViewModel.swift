456499be23eea3d9b4f12a518b5e9cae246ac1f3
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-19 21:45:47 +0300

    [Fix] CartViewModel nftCards/APIResponsesStatus implementation

--- iOS-FakeNFT-Extended/UI/Cart/CartViewModel.swift
+++ iOS-FakeNFT-Extended/UI/Cart/CartViewModel.swift
@@ -10,18 +10,41 @@ import SwiftUI
 @MainActor
 @Observable
 final class CartViewModel {
-    var nftCards: [NftCardItem] = NftCardItem.mockItems
+    var status: APIResponseStatus = .default
+    var nftCards: [Nft] = []
     var totalCount: String {
         nftCards.count.description + " NFT"
     }
     var totalPrice: String {
-        nftCards.reduce(0) { $0 + $1.price }.description + " " +
-        (nftCards.isEmpty ? "" : String(describing: nftCards[0].currency))
+        let price = nftCards.reduce(0) { $0 + $1.price }
+        return String(format: "%.2f", price) + " " + (nftCards.isEmpty ? "" : "ETH")
+    }
+    
+    var emptyCart: Bool {
+        status == .success && nftCards.isEmpty
     }
                 
-    func remove(item: NftCardItem) {
-        if let index = nftCards.firstIndex(of: item) {
-            nftCards.remove(at: index)
+    func remove(item: Nft) {
+        nftCards.removeAll(where: { $0.id == item.id })
+    }
+    
+    func load() async {
+        var result: NftOrder
+        do {
+            status = .loading
+            result = try await DefaultNetworkClient().send(request: NftByOrderRequest())
+            let nfts = result.nfts.isEmpty ? NftOrder.mock : result.nfts
+            
+            nftCards.removeAll()
+            
+            for id in nfts {
+                let nft: Nft = try await DefaultNetworkClient().send(request: NFTRequest(id: id))
+                nftCards.append(nft)
+            }
+            status = .success
+        } catch {
+            print(error)
+            status = .failure
         }
     }
 }
