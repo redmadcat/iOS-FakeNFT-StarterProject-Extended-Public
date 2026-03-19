d6c2a8b4b395c67da810db4d9ca43b7ecd75e212
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-19 14:38:33 +0300

    [Add] Nft implementation

--- iOS-FakeNFT-Extended/Models/Network/Nft.swift
+++ iOS-FakeNFT-Extended/Models/Network/Nft.swift
@@ -2,5 +2,12 @@ import Foundation
 
 struct Nft: Decodable {
     let id: String
+    let createdAt: String
+    let name: String
     let images: [URL]
+    let rating: Int
+    let description: String
+    let price: Float
+    let author: String
+    let website: String
 }
