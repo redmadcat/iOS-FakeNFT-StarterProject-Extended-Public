3575187a0066c7624423d75c0b031641b5b1c0bc
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-23 12:43:04 +0300

    [Add] 2 columns LazyVGrid layout definition

--- iOS-FakeNFT-Extended/UI/Payment/PaymentMethodView.swift
+++ iOS-FakeNFT-Extended/UI/Payment/PaymentMethodView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct PaymentMethodView: View {
+    
+    private static let columns = [
+        GridItem(.flexible(), spacing: 7),
+        GridItem(.flexible(), spacing: 7)
+    ]
+    
     var body: some View {
         VStack {
-            
+            ScrollView {
+                LazyVGrid(columns: Self.columns, spacing: 7) {
+                    
+                }
+            }
         }
         .navigationTitle("PaymentMethodView.navigation.title")
         .navigationBarBackButtonHidden(true)
