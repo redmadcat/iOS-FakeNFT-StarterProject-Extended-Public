d1c4b9a7d0b8022f99d89d0bf07262d560db147b
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-23 12:24:55 +0300

    [Add] NavigationBackButton implementation

---
+++ iOS-FakeNFT-Extended/UI/Navigation/NavigationBackButton.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationBackButton.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 23.03.2026.
+//
+
+import SwiftUI
+
+struct NavigationBackButton: View {
+    @Environment(\.dismiss) private var dismiss
+    var action: (() -> Void)? = nil
+    
+    var body: some View {
+        Button(action: {
+            dismiss()
+        }) {
+            Image(systemName: "chevron.left")
+                .foregroundColor(.ypBlackAD)
+        }
+    }
+}
+
+#Preview {
+    NavigationBackButton()
+}
