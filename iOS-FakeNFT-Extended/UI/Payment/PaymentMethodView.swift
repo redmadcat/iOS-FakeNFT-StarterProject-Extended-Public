ec9beec2494c0da3974236251c7df55f01fde046
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-23 12:01:05 +0300

    [Add] PaymentMethodView navigation layout definition

---
+++ iOS-FakeNFT-Extended/UI/Payment/PaymentMethodView.swift
@@ -0,0 +1,32 @@
+//
+//  PaymentMethodView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 23.03.2026.
+//
+
+import SwiftUI
+
+struct PaymentMethodView: View {
+    var body: some View {
+        VStack {
+   
+        }
+        .navigationTitle("PaymentMethodView.navigation.title")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+
+            }
+        }
+        .background(.ypWhiteAD)
+        .task {
+
+        }
+    }
+}
+
+#Preview {
+    PaymentMethodView()
+}
