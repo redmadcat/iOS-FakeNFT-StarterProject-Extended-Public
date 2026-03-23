beb9ecf38abb07509a2507e87a1ba42b592c9fa9
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-23 09:00:02 +0300

    [Add] NftOrderPredicate helper enum implementation

---
+++ iOS-FakeNFT-Extended/Models/Helpers/NftOrderPredicate.swift
@@ -0,0 +1,12 @@
+//
+//  NftOrderPredicate.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Roman Yaschenkov on 23.03.2026.
+//
+
+enum NtfOrderPredicate {
+    case price
+    case rating
+    case name
+}
