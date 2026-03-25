f40c40cac56847ae1d7fc727f8fd19dc73aa94cb
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-25 15:01:44 +0300

    [Fix] PaymentMethodViewModel remove deprecated pay member

--- iOS-FakeNFT-Extended/UI/Payment/PaymentMethodViewModel.swift
+++ iOS-FakeNFT-Extended/UI/Payment/PaymentMethodViewModel.swift
@@ -28,23 +28,4 @@ final class PaymentMethodViewModel {
             status = .failure
         }
     }
-    
-    func pay(currency: Currency?) async {
-        guard let currency else { return }
-
-        do {
-            status = .loading
-            let response = try await service.pay(currency: currency)
-            print(response)
-            status = .success
-            // TODO: clear cart
-        } catch PaymentTransaction.failed {
-            status = .failure
-            // TODO: show modal
-        } catch {
-            print(error.localizedDescription)
-            status = .failure
-            // TODO: show modal
-        }
-    }
 }
