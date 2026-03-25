60cc905f26b9756f4c771346d31d320dd8ae0e34
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-25 14:50:09 +0300

    [Fix] CurrencyService remove deprecated pay member

--- iOS-FakeNFT-Extended/Services/CurrencyService.swift
+++ iOS-FakeNFT-Extended/Services/CurrencyService.swift
@@ -7,7 +7,6 @@
 
 protocol CurrencyService {
     func load() async throws -> [Currency]
-    func pay(currency: Currency) async throws -> PaymentResponse
 }
 
 @MainActor
@@ -29,16 +28,5 @@ final class CurrencyServiceImpl: CurrencyService {
             await storage.save(currency)
         }
         return await storage.cache
-    }
-    
-    func pay(currency: Currency) async throws -> PaymentResponse {
-        let request = PaymentRequest(currencyId: currency.id)
-        let response: PaymentResponse = try await networkClient.send(request: request)
-        
-        guard response.success else {
-            throw PaymentTransaction.failed
-        }
-        
-        return response
     }
 }
