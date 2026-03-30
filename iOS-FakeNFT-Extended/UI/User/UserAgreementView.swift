130bfd63d8d8f8ce5b7c47a0824d4b6e3221d472
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-30 16:15:20 +0300

    [Fix] remove redundant WebView implementation

--- iOS-FakeNFT-Extended/UI/User/UserAgreementView.swift
+++ iOS-FakeNFT-Extended/UI/User/UserAgreementView.swift
@@ -37,26 +37,6 @@ struct UserAgreementView: View {
     }
 }
 
-struct WebView: UIViewRepresentable {
-    let url: URL
-    @Binding var isLoading: Bool
-    
-    func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.navigationDelegate = context.coordinator
-        return webView
-    }
-    
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-}
-
 #Preview {
     UserAgreementView()
 }
