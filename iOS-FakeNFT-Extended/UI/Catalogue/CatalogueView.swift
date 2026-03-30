f4838746adbe0aa8e89d309628b109003e345213
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-03-30 12:57:51 +0300

    [Fix] navigation based on Router member

--- iOS-FakeNFT-Extended/UI/Catalogue/CatalogueView.swift
+++ iOS-FakeNFT-Extended/UI/Catalogue/CatalogueView.swift
@@ -8,88 +8,72 @@
 import SwiftUI
 
 struct CatalogueView: View {
-     let vm : CatalogueViewModel
     @State private var showSort = false
-    @State private var path: [SelectionType] = []
-    var body: some View {
-        NavigationStack(path: $path) {
-            VStack{
-                HStack{
-                    Spacer()
-                    Button(action: {
-                        showSort = true
-                    }, label: {
-                        Image(.sortIcon)
-                    })
-                    .frame(width: 42, height: 42)
-                }
-                .frame(height: 42)
-                ZStack{
-                    if vm.collections.isEmpty {
-                        ProgressView()
-                    }
-                    ScrollView(showsIndicators: false) {
-                        LazyVStack(spacing: 8) {
-                            ForEach(vm.collections) { collection in
-                                Button{
-                                    let nfts =  vm.mapNftsToCollection(collection: collection)
-                                    path.append(.collectionNft(collection, nfts))
-                                }label:{
-                                    CatalogueListRowView(
-                                        imageURL: collection.cover,
-                                        title: collection.name,
-                                        imageCount: collection.nfts.count
-                                    )
-                                }
-                                .buttonStyle(.plain)
-                            }
-                        }
-                        .padding(.top, 20)
-                    }
-                }
-            }
-            .padding()
-            .confirmationDialog(
-                "Сортировка",
-                isPresented: $showSort,
-                titleVisibility: .visible
-            ) {
-                Button("По названию") {
-                    Task {
-                        await vm.sortByName()
-                    }
-                }
-                
-                Button("По количеству NFT") {
-                    Task {
-                        await vm.sortByCount()
-                    }
-                }
-                
-                Button("Закрыть", role: .cancel) { }
-            }
+    let vm : CatalogueViewModel
 
-            .navigationDestination(for: SelectionType.self) { type in
-                switch type {
-                case .collectionNft(let collection, let nfts):
-                    CollectionNFTView(vm: CollectionNFTViewModel(collection: collection, nfts: nfts, serviceAssembly: vm.serviceAssembly) ,path: $path
-                    )
-                case .webView(let url):
-                    if let webURL = URL(string: url) {
-                        WebViewContainer(url: webURL, path: $path)
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    showSort = true
+                }, label: {
+                    Image(.sortIcon)
+                })
+                .padding(.trailing, 10)
+                .frame(width: 42, height: 42)
+                .buttonStyle(.plain)
+            }
+            ZStack {
+                if vm.collections.isEmpty {
+                    ProgressView()
+                }
+                ScrollView(showsIndicators: false) {
+                    LazyVStack(spacing: 8) {
+                        ForEach(vm.collections) { collection in
+                            Button{
+                                let nfts =  vm.mapNftsToCollection(collection: collection)
+                                Router.shared.toCollection(collection: collection, nfts: nfts)
+                            }label: {
+                                CatalogueListRowView(
+                                    imageURL: collection.cover,
+                                    title: collection.name,
+                                    imageCount: collection.nfts.count
+                                )
+                            }
+                            .buttonStyle(.plain)
+                        }
                     }
-                   
+                    .padding(.top, 20)
+                    .padding(.horizontal, 16)
+                }
+            }
+        }
+        .confirmationDialog(
+            "ActionSheet.sorting.title",
+            isPresented: $showSort,
+            titleVisibility: .visible
+        ) {
+            Button("ActionSheet.sorting.name") {
+                Task {
+                    await vm.sortByName()
                 }
             }
             
+            Button("ActionSheet.sorting.count") {
+                Task {
+                    await vm.sortByCount()
+                }
+            }
+            
+            Button("ActionSheet.sorting.close", role: .cancel) { }
         }
     }
 }
 
 
 #Preview {
-  
-   let vm = CatalogueViewModel(
+    let vm = CatalogueViewModel(
     serviceAssembly: ServicesAssembly(networkClient: DefaultNetworkClient()))
     CatalogueView(vm: vm)
 }
